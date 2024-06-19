49e34a306e0ec4606163a829ae89d3e51754a616
Fisch37 <seracin4@gmail.com>
2024-06-19 11:27:50 +0200

    Added automatic owner suggestions for /wps go

--- src/main/java/com/cimadev/cimpleWaypointSystem/command/WaypointSuggestionProvider.java
+++ src/main/java/com/cimadev/cimpleWaypointSystem/command/WaypointSuggestionProvider.java
@@ -1,4 +1,6 @@
 package com.cimadev.cimpleWaypointSystem.command;
+import com.cimadev.cimpleWaypointSystem.command.persistentData.AccessLevel;
+import com.cimadev.cimpleWaypointSystem.command.persistentData.OfflinePlayer;
 import com.cimadev.cimpleWaypointSystem.command.persistentData.Waypoint;
 
 import com.mojang.brigadier.context.CommandContext;
@@ -8,11 +10,22 @@ import com.mojang.brigadier.suggestion.Suggestions;
 import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.concurrent.CompletableFuture;
 
 public class WaypointSuggestionProvider implements SuggestionProvider<ServerCommandSource> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(WaypointSuggestionProvider.class);
+
+    private final boolean withOwner;
+
+    public WaypointSuggestionProvider() { this(false); }
+    public WaypointSuggestionProvider(boolean withOwner) {
+        this.withOwner = withOwner;
+    }
+
     @Override
     public CompletableFuture<Suggestions> getSuggestions(CommandContext<ServerCommandSource> context, SuggestionsBuilder builder) throws CommandSyntaxException {
         /* TODO: Implement more consistent suggestions.
@@ -33,7 +46,20 @@ public class WaypointSuggestionProvider implements SuggestionProvider<ServerComm
         ServerPlayerEntity player = context.getSource().getPlayer();
         ArrayList<Waypoint> waypoints = WpsUtils.getAccessibleWaypoints(player,null, false, false);
         for (Waypoint waypoint : waypoints) {
-            builder.suggest(waypoint.getName());
+            String suggestion;
+            if (!withOwner || (player != null && player.getUuid().equals(waypoint.getOwner())))
+                suggestion = waypoint.getName();
+            else if (waypoint.getAccess() == AccessLevel.OPEN)
+                suggestion = waypoint.getName() + " open";
+            else {
+                OfflinePlayer owner = waypoint.getOwnerPlayer();
+                if (owner == null) {
+                    LOGGER.warn("Waypoint {} has unknown owner", waypoint.getName());
+                    continue;
+                }
+                suggestion = waypoint.getName() + " " + owner.getName();
+            }
+            builder.suggest(suggestion);
         }
 
         return builder.buildFuture();
